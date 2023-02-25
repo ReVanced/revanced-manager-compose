22835f1a95d2654a1501a0db7ab7262a96565c58
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2023-02-25 20:31:05 +0700

    chore(deps): bump gradle from 7.6 -> 8.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "7.3.1" apply false
-    id("com.android.library") version "7.3.1" apply false
+    id("com.android.application") version "7.4.1" apply false
+    id("com.android.library") version "7.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
 }
 repositories {
