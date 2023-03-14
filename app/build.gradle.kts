5586cd3c01f7791faf9775502c7490e30a09618f
Patryk Mis <patryk.mis@mykolab.com>
2023-03-14 15:27:58 +0100

    build: Bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 android {
     namespace = "app.revanced.manager.compose"
     compileSdk = 33
-    buildToolsVersion = "33.0.0"
+    buildToolsVersion = "33.0.2"
 
     defaultConfig {
         applicationId = "app.revanced.manager.compose"
@@ -40,28 +40,28 @@ android {
 
     buildFeatures.compose = true
 
-    composeOptions.kotlinCompilerExtensionVersion = "1.3.2"
+    composeOptions.kotlinCompilerExtensionVersion = "1.4.3"
 }
 
 dependencies {
 
     // AndroidX Core
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("androidx.activity:activity-compose:1.6.1")
 
     // Compose
-    val composeVersion = "1.3.3"
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
 
     // Material 3
-    implementation("androidx.compose.material3:material3:1.0.1")
+    implementation("androidx.compose.material3:material3")
 
     // Koin
-    implementation("io.insert-koin:koin-android:3.3.2")
-    implementation("io.insert-koin:koin-androidx-compose:3.4.1")
+    implementation("io.insert-koin:koin-android:3.3.3")
+    implementation("io.insert-koin:koin-androidx-compose:3.4.2")
 
     // Compose Navigation
     implementation("dev.olshevski.navigation:reimagined:1.3.1")
