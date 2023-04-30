c2a35b0b51ceec8f2d75619466703df770ca1f36
CnC-Robert <CnC.Rob3rt@gmail.com>
2023-04-30 19:26:04 +0000

    refactor: remove AppBar

--- app/src/main/java/app/revanced/manager/compose/ui/screen/DashboardScreen.kt
+++ app/src/main/java/app/revanced/manager/compose/ui/screen/DashboardScreen.kt
@@ -7,20 +7,24 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import app.revanced.manager.compose.R
-import app.revanced.manager.compose.ui.component.AppBar
 import kotlinx.coroutines.launch
 
 enum class DashboardPage(
@@ -40,7 +44,22 @@ fun DashboardScreen() {
     val coroutineScope = rememberCoroutineScope()
 
     Scaffold(
-        topBar = { AppBar("ReVanced Manager") },
+        topBar = {
+            TopAppBar(
+                title = { Text("ReVanced Manager") },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Outlined.Info, contentDescription = null)
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Outlined.Notifications, contentDescription = null)
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Outlined.Settings, contentDescription = null)
+                    }
+                }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = {}) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
