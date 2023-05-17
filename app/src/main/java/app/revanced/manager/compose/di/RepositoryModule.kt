b3aee3f41641fb7f3a7304f584e84503c06188b1
Ax333l <main@axelen.xyz>
2023-05-17 16:26:07 +0200

    refactor: merge bundlerepository into patchesrepository for now

--- app/src/main/java/app/revanced/manager/compose/di/RepositoryModule.kt
+++ app/src/main/java/app/revanced/manager/compose/di/RepositoryModule.kt
@@ -9,6 +9,5 @@ import org.koin.dsl.module
 val repositoryModule = module {
     singleOf(::ReVancedRepositoryImpl)
     singleOf(::ManagerAPI)
-    singleOf(::BundleRepository)
     singleOf(::PatchesRepository)
 }
--- app/src/main/java/app/revanced/manager/compose/patcher/data/repository/PatchesRepository.kt
+++ app/src/main/java/app/revanced/manager/compose/patcher/data/repository/PatchesRepository.kt
@@ -1,12 +1,20 @@
 package app.revanced.manager.compose.patcher.data.repository
 
-// TODO: this makes more sense in the "domain layer" as it is right now.
-class PatchesRepository(private val bundleRepository: BundleRepository) {
-    // private val bundle = PatchBundleDataSource(testingPatchBundle, null)
+import app.revanced.manager.compose.network.api.ManagerAPI
+import app.revanced.manager.compose.patcher.data.PatchBundleDataSource
+
+// TODO: this might make more sense in the "domain" layer.
+class PatchesRepository(private val managerAPI: ManagerAPI) {
+    private var bundle: PatchBundleDataSource? = null
+
+    private suspend fun getBundle() = bundle ?: PatchBundleDataSource(
+        managerAPI.downloadPatchBundle()!!.absolutePath,
+        managerAPI.downloadIntegrations()
+    ).also { bundle = it }
 
     suspend fun patchClassesFor(packageName: String, packageVersion: String) =
-        bundleRepository.getBundle().getPatchesFiltered(packageName, packageVersion)
+        getBundle().getPatchesFiltered(packageName, packageVersion)
 
     // TODO: move this out of here.
-    suspend fun getIntegrations() = listOfNotNull(bundleRepository.getBundle().integrations)
+    suspend fun getIntegrations() = listOfNotNull(getBundle().integrations)
 }
