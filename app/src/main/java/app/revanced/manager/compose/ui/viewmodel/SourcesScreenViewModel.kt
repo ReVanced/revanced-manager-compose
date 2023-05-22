82445c9181de91df48c69ce3246c28fbdd16091c
Ax333l <main@axelen.xyz>
2023-05-22 15:39:45 +0200

    string resource for the toast

--- app/src/main/java/app/revanced/manager/compose/ui/viewmodel/SourcesScreenViewModel.kt
+++ app/src/main/java/app/revanced/manager/compose/ui/viewmodel/SourcesScreenViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import app.revanced.manager.compose.R
 import app.revanced.manager.compose.domain.repository.SourcesProvider
 import app.revanced.manager.compose.util.toast
 import kotlinx.coroutines.launch
@@ -15,7 +16,7 @@ class SourcesScreenViewModel(private val app: Application, private val sourcesPr
         try {
             sourcesProvider.reloadSources()
         } catch (err: Throwable) {
-            app.toast("Failed to update patch bundles")
+            app.toast(app.getString(R.string.source_download_fail))
             Log.e("revanced-manager", "Failed to update patch bundles", err)
         }
     }
