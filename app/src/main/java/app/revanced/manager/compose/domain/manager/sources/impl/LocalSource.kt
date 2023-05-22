fee9d4bd418528beb2616af24d2b3c4603f57846
Ax333l <main@axelen.xyz>
2023-05-22 19:05:10 +0200

    implement the function that allows the user to replace the files

--- app/src/main/java/app/revanced/manager/compose/domain/manager/sources/impl/LocalSource.kt
+++ app/src/main/java/app/revanced/manager/compose/domain/manager/sources/impl/LocalSource.kt
@@ -2,8 +2,11 @@ package app.revanced.manager.compose.domain.manager.sources.impl
 
 import app.revanced.manager.compose.domain.manager.patch.PatchBundle
 import app.revanced.manager.compose.domain.manager.sources.Source
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.withContext
 import java.io.File
+import java.nio.file.Files
 
 class LocalSource(directory: File) : Source(directory) {
     override val bundle = MutableStateFlow(loadBundle())
@@ -15,5 +18,18 @@ class LocalSource(directory: File) : Source(directory) {
         emptyPatchBundle
     }
 
-    // TODO: write function that allows the user to replace the files
+    suspend fun replace(patches: File? = null, integrations: File? = null) {
+        withContext(Dispatchers.IO) {
+            patches?.let {
+                Files.copy(it.toPath(), patchesJar.toPath())
+            }
+            integrations?.let {
+                Files.copy(it.toPath(), this@LocalSource.integrations.toPath())
+            }
+        }
+
+        withContext(Dispatchers.Main) {
+            bundle.emit(loadBundle { throw it })
+        }
+    }
 }
