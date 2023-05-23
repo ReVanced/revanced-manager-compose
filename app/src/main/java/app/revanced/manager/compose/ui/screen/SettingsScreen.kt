b5d248be595aef9cb0b6ff5618230a4a156af7df
Robert <72943079+CnC-Robert@users.noreply.github.com>
2023-05-23 08:49:58 +0000

    refactor: remove redundant `getViewModel`
    Co-authored-by: Ax333l <main@axelen.xyz>

--- app/src/main/java/app/revanced/manager/compose/ui/screen/SettingsScreen.kt
+++ app/src/main/java/app/revanced/manager/compose/ui/screen/SettingsScreen.kt
@@ -118,7 +118,6 @@ fun SettingsScreen(
 
             is SettingsDestination.UpdateProgress -> UpdateProgressScreen(
                { navController.pop() },
-                getViewModel<UpdateSettingsViewModel>()
             )
 
             is SettingsDestination.Settings -> {
