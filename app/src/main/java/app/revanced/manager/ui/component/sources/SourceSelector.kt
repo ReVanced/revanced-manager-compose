83dab7f0febb0eac14d350128d14093cf0a87d6b
Ax333l <main@axelen.xyz>
2023-06-22 10:36:17 +0200

    make the selector look better

--- app/src/main/java/app/revanced/manager/ui/component/sources/SourceSelector.kt
+++ app/src/main/java/app/revanced/manager/ui/component/sources/SourceSelector.kt
@@ -1,15 +1,20 @@
 package app.revanced.manager.ui.component.sources
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import app.revanced.manager.domain.sources.Source
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,18 +33,40 @@ fun SourceSelector(sources: List<Source>, onFinish: (Source?) -> Unit) {
     ModalBottomSheet(
         onDismissRequest = { onFinish(null) }
     ) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(
-                text = "Select bundle",
-                style = MaterialTheme.typography.titleMedium
-            )
-            sources.forEach {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .height(48.dp)
+                    .fillMaxWidth()
+            ) {
                 Text(
-                    text = it.name,
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    modifier = Modifier.clickable { onFinish(it) }
+                    text = "Select bundle",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSurface
                 )
+            }
+            sources.forEach {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .height(48.dp)
+                        .fillMaxWidth()
+                        .clickable {
+                            onFinish(it)
+                        }
+                ) {
+                    Text(
+                        text = it.name,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
             }
         }
     }
