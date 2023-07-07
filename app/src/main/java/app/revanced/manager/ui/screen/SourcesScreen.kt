2195e6c29c248e6b1ea77e5b5f5aa42c4ca9b391
Tyff <tyffhabwe@gmail.com>
2023-07-08 01:23:44 +1200

    feat: making proper Bundle pages - start without code improvements - base import bundles page works and looks decent - now need to extend the ImportBundleDialog to be usable multiple times depending on who's calling it - have tried to make it a bit more modular but most of that work will happen in next commit - Now have most of the Bundle information page with correct colour/font sizes etc. Next commit should just be Bundle Patches page then start to PR

--- app/src/main/java/app/revanced/manager/ui/screen/SourcesScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/SourcesScreen.kt
@@ -1,16 +1,23 @@
 package app.revanced.manager.ui.screen
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
 import app.revanced.manager.R
 import app.revanced.manager.ui.component.sources.ImportBundleDialog
-import app.revanced.manager.ui.component.sources.NewSourceDialog
 import app.revanced.manager.ui.component.sources.SourceItem
 import app.revanced.manager.ui.viewmodel.SourcesViewModel
 import org.koin.androidx.compose.getViewModel
@@ -22,32 +29,26 @@ fun SourcesScreen(
     var showNewSourceDialog by rememberSaveable { mutableStateOf(false) }
     val sources by vm.sources.collectAsStateWithLifecycle(initialValue = emptyList())
 
-    if (showNewSourceDialog)  {
-        /**
-        NewSourceDialog(
-            onDismissRequest = { showNewSourceDialog = false },
-            onLocalSubmit = { name, patches, integrations ->
-                showNewSourceDialog = false
-                vm.addLocal(name, patches, integrations)
-            },
-            onRemoteSubmit = { name, url ->
-                showNewSourceDialog = false
-                vm.addRemote(name, url)
-            }
-        )*/
+    if (showNewSourceDialog) {
         ImportBundleDialog(
             onDismissRequest = { showNewSourceDialog = false },
             onLocalSubmit = { name, patches, integrations ->
                 showNewSourceDialog = false
                 vm.addLocal(name, patches, integrations)
             },
+            onBackIcon = {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = null
+                )
+            },
+            topBarTitle = stringResource(R.string.import_bundle),
             onRemoteSubmit = { name, url ->
                 showNewSourceDialog = false
                 vm.addRemote(name, url)
-            }
+            },
         )
     }
-
 
     Column(
         modifier = Modifier
